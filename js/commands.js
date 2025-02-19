32502968483dca3c470b2b83f02e1e4a74e08f14
Abyalew Kebede <121717429+Anorie1@users.noreply.github.com>
2025-02-19 12:33:04 +0300

    update some feature

---
+++ js/commands.js
@@ -0,0 +1,131 @@
+let linkedin = "https://www.linkedin.com/in/abyalew-kebede";
+let github = "https://github.com/Anorie1";
+let website = "https://abyalew.live";
+let instagram = "https://www.instagram.com/Abyalew_Kebede";
+let repo = "https://github.com/Anorie1/terminal_portfolio";
+let project = "https://github.com/Anorie1?tab=repositories";
+let project_site = "https://index.html";
+let existCommands = [
+  "whoami",
+  "banner",
+  "clear",
+  "cls",
+  "date",
+  "github",
+  "techstack",
+  "gui",
+  "help",
+  "history",
+  "linkedin",
+  "project",
+  "education",
+  "platforms",
+  "theme",
+  "theme ls",
+  "theme set",
+  "vistor",
+  "cd ..",
+];
+
+whoami = [
+  "&nbsp;",
+  "&nbsp;",
+  " Hey!👋, I'm <span style='color: yellow;'>Abyalew Kebede</span> ",
+  "&nbsp;",
+  "A Fullstack developer and penetration tester. Learn and",
+  "explore new technologies and share the knowledge back to the community.",
+
+  "&nbsp;",
+  "&nbsp;",
+  "Connect With me ",
+  "&nbsp;",
+  // '<p align="center">',
+
+  ' <a href="https://www.abyalew42@gmail.com" target="blank">Gmail</a>',
+
+  "&nbsp;",
+  "&nbsp;",
+];
+
+education = [
+  "BSC degree in Electrical and computer engineering",
+  "Graduted from Mattu University, Ethiopia",
+];
+
+resume = [
+  " Here my resume, if you wanna see and download it",
+  '<a href="">Download resume</a>',
+];
+
+techstack = [
+  '<span style="color: blue;" > Frontend </span>: <span style="color: yellow;" > HTML,</span>  CSS , <span style="color: yellow;" > JavaScript,</span>  React, <span style="color: yellowgreen;" > Tailwindcss</span>',
+  "Backend : Nodejs, Express, Python, Django",
+  '<span style="color: pink;" > Database</span> : MonogoDB, <span style="color: brown;" >Postgre</span>, MySQL',
+  "Other : Adobe photoshop, Kali linux, ",
+];
+
+platforms = [
+  "TO open the below social profiles Type the desired one!",
+  "Example : github",
+  "----------------",
+  "&nbsp;",
+  '<a href="https://github.com/Anorie1" style="color: skyblue;">Github</a>',
+
+  '<a href="https://www.linkedin.com/in/abyalew-kebede" style="color: skyblue;">Linkedin</a>',
+  '<a href="https://www.instagram.com/Abyalew_Kebede" style="color: skyblue;">Instagram</a>',
+];
+
+themes = ["dark", "light"];
+
+theme = [
+  "Usage: theme [arg]",
+  "Args:",
+  " - ls: list all available themes",
+  " - set: set a theme",
+  "<br>",
+  "Example:",
+  " theme ls # to list all available themes",
+  ' <span style="color:skyblue;">theme set light</span> # to set a theme',
+];
+
+help = [
+  '<span class="command">whoami  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  - </span><span class="helpdes">about me</span>',
+  '<span class="command">banner &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - </span> <span class="helpdes">Display the header</span>',
+  '<span class="command">clear | cls  &nbsp;&nbsp;&nbsp; - </span>  <span class="helpdes">   Clear the teminal</span>',
+  '<span class="command">date  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;         - </span> <span class="helpdes">    Display the actual date</span>',
+  '<span class="command">gui  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;          - </span>    <span class="helpdes"> Open the Portfolio Website</span>',
+  '<span class="command">help  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;         - </span>     <span class="helpdes">Check available commands</span>',
+  '<span class="command">history  &nbsp&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;      - </span> <span class="helpdes">    View command history</span>',
+  '<span class="command">project &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;      - </span>    <span class="helpdes"> View coding projects</span>',
+  '<span class="command">education  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;      - </span>    <span class="helpdes"> Open the source code</span>',
+  '<span class="command">techstack  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;      - </span>    <span class="helpdes"> See the tech stack</span>',
+  '<span class="command">resume  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;      - </span>    <span class="helpdes"> See and download resume it</span>',
+  '<span class="command">platforms &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - </span>  <span class="helpdes">   Display used internet platforms</span>',
+  '<span class="command">theme    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;      - </span>    <span class="helpdes"> Theme configurations</span>',
+  '<span class="command">pwd  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;          - </span>    <span class="helpdes"> Print present working directory</span>',
+  "<br>",
+  "[TAB]              Trigger completion",
+  "[Ctrl+l]           Clear terminal",
+  "[Ctrl+p]           Go back to previous command",
+];
+
+banner = [
+  '<span style="color: yellowgreen;">visitor@terminal.abyalew:~$</span> Welcome',
+  " &nbsp;",
+  " &nbsp;",
+  " ",
+
+  "╔══╦╗───────────── ╔╦╗─╔╗───╔╗",
+  "║╔╗║╚╦╦╦═╗╔╗╔═╦╦╦╗ ║╔╬═╣╚╦═╦╝╠═╗",
+  "║╠╣║╬║║║╬╚╣╚╣╩╣║║║ ║╚╣╩╣╬║╩╣╬║╩╣",
+  "╚╝╚╩═╬╗╠══╩═╩═╩══╝ ╚╩╩═╩═╩═╩═╩═╝",
+  "─────╚═╝",
+  "&nbsp;",
+  "&nbsp;",
+  " Hey! 👋, I'm <span style='color: blue;'>Abyalew Kebede</span> ",
+  "Welcome To my Terminal Portfolio !",
+  " ==================================",
+  "&nbsp;",
+
+  "For a list of available commands, type<span class=\"command\">'help'</span> ",
+];
